880937fc6682447eb88dc47f722fb2eb2279cd4d
stvali2 <stvali2@gmail.com>
2017-10-14 10:18:22 +0200

    class structure

---
+++ src/CommunityCard.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Nancy.Simple
+{
+
+	public class CommunityCard
+	{
+		public string rank { get; set; }
+		public string suit { get; set; }
+	}
+}
---
+++ src/Player.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Nancy.Simple
+{
+
+	public class Player
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public string status { get; set; }
+		public string version { get; set; }
+		public int stack { get; set; }
+		public int bet { get; set; }
+		public List<HoleCard> hole_cards { get; set; }
+	}
+}

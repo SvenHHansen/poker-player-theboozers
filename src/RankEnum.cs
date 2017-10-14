ea1a558947e1bcf44d438ffc586fc3fb51d3c3c7
Frank Roth <frank-roth@outlook.com>
2017-10-14 13:36:37 +0200

    Added Rank and Suit enums Added Enums in parallel to Card class

---
+++ src/RankEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    public enum RankEnum
+    {
+        diamonds = 0,
+        hearts,
+        spades,
+        clubs
+    }
+}

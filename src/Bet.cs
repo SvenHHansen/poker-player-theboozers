f22132fac7e8f796708071ef0fcced9b9383c620
Mathias Dietz <mathias@moni-mathias.de>
2017-10-14 14:15:58 +0200

    Mappinganpassungen

--- src/Bet.cs
+++ src/Bet.cs
@@ -13,7 +13,17 @@ namespace Nancy.Simple
             {
                 case HandEnum.RoyalFlush:
                 case HandEnum.StraightFlush:
+                case HandEnum.FourOfAKind:
+                case HandEnum.FullHouse:
+                case HandEnum.Flush:
+                case HandEnum.Straight:
                     return cash;
+                case HandEnum.ThreeOfAKind:
+                case HandEnum.TwoPairs:
+                    return cash/2;
+                case HandEnum.OnePair:
+                    return cash/4;
+         
                 default:
                     return cash / 20;
             }

596af4a6a4cb3bb6aef1440e558c85fa183f9de3
Frank Roth <frank-roth@outlook.com>
2017-10-14 10:55:50 +0200

    Add empty DeckClassification and DeckStub

---
+++ src/DeckClassification.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    class DeckStub
+    {
+
+    }
+
+    public class DeckClassification
+    {
+        public bool IsRoyalFlush(DeckStub deck)
+        {
+
+        }
+
+        public bool IsStraightFlush(DeckStub deck)
+        {
+
+        }
+
+        public bool IsFourOfAKind(DeckStub deck)
+        {
+
+        }
+
+        public bool IsFullHouse(DeckStub deck)
+        {
+
+        }
+
+        public bool IsFlush(DeckStub deck)
+        {
+
+        }
+
+        public bool IsStraight(DeckStub deck)
+        {
+
+        }
+
+        public bool IsThreeOfAKind(DeckStub deck)
+        {
+
+        }
+
+        public bool IsTwoPair(DeckStub deck)
+        {
+
+        }
+
+        public bool IsOnePair(DeckStub deck)
+        {
+
+        }
+
+        public bool IsHighestCard(DeckStub deck)
+        {
+
+        }
+
+    }
+}
